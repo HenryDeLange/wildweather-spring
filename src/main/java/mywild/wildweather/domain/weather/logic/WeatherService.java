9e3d840e978e35a12ddcc1af64d7ea9907bcb02d
Henry <36891536+HenryDeLange@users.noreply.github.com>
2025-10-29 08:50:20 +0200

    better wind direction mapping

--- src/main/java/mywild/wildweather/domain/weather/logic/WeatherService.java
+++ src/main/java/mywild/wildweather/domain/weather/logic/WeatherService.java
@@ -45,14 +45,45 @@ public class WeatherService {
         FIELD_EXTRACTORS = Collections.unmodifiableMap(fieldMappings);
     }
 
-    private static double mapDirection(String direction) {
+    private static Double mapDirection(String direction) {
         if (direction == null || direction.isBlank())
             return 0.0;
-        return Double.parseDouble(direction
-            .replace('N', '1')
-            .replace('E', '2')
-            .replace('S', '3')
-            .replace('W', '4'));
+        switch (direction) {
+            case "N":
+                return 0.0;
+            case "NNE":
+                return 22.5;
+            case "NE":
+                return 45.0;
+            case "ENE":
+                return 67.5;
+            case "E":
+                return 90.0;
+            case "ESE":
+                return 112.5;
+            case "SE":
+                return 135.0;
+            case "SSE":
+                return 157.5;
+            case "S":
+                return 180.0;
+            case "SSW":
+                return 202.5;
+            case "SW":
+                return 225.0;
+            case "WSW":
+                return 247.5;
+            case "W":
+                return 270.0;
+            case "WNW":
+                return 292.5;
+            case "NW":
+                return 315.0;
+            case "NNW":
+                return 337.5;
+            default:
+                return 0.0;
+        }
     }
 
     @Autowired
